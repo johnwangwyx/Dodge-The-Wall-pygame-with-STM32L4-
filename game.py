e2be94ac11cf1a70ae18f4d5919d153e7ec08b97
John Wang <yuxuan.wang@mail.mcgill.ca>
2023-10-27 18:09:40 -0400

    Comments and Up/Down movement

--- game.py
+++ game.py
@@ -2,17 +2,17 @@ import pygame
 import random
 from datetime import datetime
 
-# Pygame initialization
+# Initialize the pygame module
 pygame.init()
 
 # Game parameters
-WIDTH, HEIGHT = 800, 600
-PLAYER_ACC = 0.9
-FRICTION = -0.12
-FPS = 60
-WALL_SPEED = 5
+WIDTH, HEIGHT = 800, 600  # Screen dimensions
+PLAYER_ACC = 0.9         # Acceleration for player movement
+FRICTION = -0.12         # Friction affecting player movement (deceleration)
+FPS = 60                 # Frames per second (game refresh rate)
+WALL_SPEED = 5           # Speed at which walls move down the screen
 
-# Colors
+# Color definitions
 WHITE = (255, 255, 255)
 RED = (213, 50, 80)
 GREEN = (0, 255, 0)
@@ -20,105 +20,131 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 BACKGROUND_COLOR = (0, 0, 0)
 
-# Player class
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 30))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-        self.vel_x = 0
+        self.image = pygame.Surface((30, 30))  # Create a rectangular player surface
+        self.image.fill(GREEN)                 # Fill the player with green color
+        self.rect = self.image.get_rect()      # Get the rectangular area covering the player
+        self.rect.center = (x, y)              # Position the player at given (x, y)
+        self.vel_x = 0  # Horizontal velocity
+        self.vel_y = 0  # Vertical velocity <-- Added
 
     def update(self, keys):
+        # Horizontal movement (left/right)
         if keys[pygame.K_LEFT]:
             self.vel_x -= PLAYER_ACC
         if keys[pygame.K_RIGHT]:
             self.vel_x += PLAYER_ACC
 
+        # Vertical movement (up/down) <-- Added
+        if keys[pygame.K_UP]:
+            self.vel_y -= PLAYER_ACC
+        if keys[pygame.K_DOWN]:
+            self.vel_y += PLAYER_ACC
+
+        # Apply friction to horizontal velocity
         self.vel_x += self.vel_x * FRICTION
+        # Update player's horizontal position
         self.rect.x += self.vel_x
 
+        # Apply friction to vertical velocity <-- Added
+        self.vel_y += self.vel_y * FRICTION
+        # Update player's vertical position <-- Added
+        self.rect.y += self.vel_y
+
+        # Ensure player doesn't move outside the screen horizontally
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
 
-# Wall class
+        # Ensure player doesn't move outside the screen vertically <-- Added
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+# Define Wall class
 class Wall(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h, existing_walls=None):
         super().__init__()
-        self.image = pygame.Surface((w, h))
-        self.image.fill(RED)
-        self.rect = self.image.get_rect(topleft=(x, y))
+        self.image = pygame.Surface((w, h))  # Create wall surface with given width and height
+        self.image.fill(RED)                 # Fill the wall with red color
+        self.rect = self.image.get_rect(topleft=(x, y))  # Position the wall at the given (x, y)
 
+        # Ensure new walls don't overlap with existing walls
         if existing_walls:
             while pygame.sprite.spritecollideany(self, existing_walls, collided=None):
                 self.rect.y = random.randint(-100, -40)
 
     def update(self):
+        # Move the wall down the screen
         self.rect.y += WALL_SPEED
+        # Remove wall from game if it moves past the bottom edge of the screen
         if self.rect.top > HEIGHT:
             self.kill()
 
-# Game loop
+# Main game loop
 def game_loop():
+    # Setup display and game title
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Dodge the Wall - Flight Simulator")
     clock = pygame.time.Clock()
 
-    all_sprites = pygame.sprite.Group()
-    walls = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()  # Group containing all sprites
+    walls = pygame.sprite.Group()        # Group containing all wall sprites
 
-    player = Player(WIDTH / 2, HEIGHT - 40)
-    all_sprites.add(player)
+    player = Player(WIDTH / 2, HEIGHT - 40)  # Instantiate player
+    all_sprites.add(player)                 # Add player to all_sprites group
 
     running = True
-    last_wall_y = 0
+    last_wall_y = 0                         # Track y-position of last generated wall
     start_time = datetime.now()
     while running:
+        # Calculate elapsed time to adjust wall speed dynamically
         current_time = datetime.now()
         elapsed_time = (current_time - start_time).total_seconds()
         global WALL_SPEED
         WALL_SPEED = 5 + elapsed_time/5
 
-        clock.tick(FPS)
+        clock.tick(FPS)  # Cap the game loop to the defined FPS
 
+        # Generate new walls if there are fewer than 5 on screen
         if len(walls) < 5:
             WALL_WIDTH = 80
             WALL_HEIGHT = random.randint(20, 40)
             random_x = random.randint(0, WIDTH - WALL_WIDTH)
-            
-            # Make sure there's a gap between this wall and the last
+            # Ensure there's a gap between consecutive walls
             gap = 70
             random_y = last_wall_y - WALL_HEIGHT - gap
             last_wall_y = random_y
-            
+
             wall = Wall(random_x, random_y, WALL_WIDTH, WALL_HEIGHT, walls)
             all_sprites.add(wall)
             walls.add(wall)
 
-        # Event handling
+        # Check for user events like closing the game window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # Update
+        # Update player and wall positions
         keys = pygame.key.get_pressed()
         player.update(keys)
-
         walls.update()
 
+        # Check for collisions between player and walls
         hits = pygame.sprite.spritecollide(player, walls, False)
         if hits:
             running = False
 
-        # Draw / render
+        # Draw all sprites on screen and update the display
         screen.fill(BACKGROUND_COLOR)
         all_sprites.draw(screen)
         pygame.display.flip()
 
-    pygame.quit()
+    pygame.quit()  # End the game
 
-# Run the game
+# Start the game loop
 game_loop()
