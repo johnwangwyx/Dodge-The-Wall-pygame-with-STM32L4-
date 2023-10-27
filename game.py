dac0c1dffb9e5a1e132b27c0950b0d31394527d1
John Wang <yuxuan.wang@mail.mcgill.ca>
2023-10-27 18:21:24 -0400

    Add collectable coins and coin tracker

--- game.py
+++ game.py
@@ -20,6 +20,8 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 BACKGROUND_COLOR = (0, 0, 0)
 
+COIN_COUNTER = 0
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -65,6 +67,18 @@ class Player(pygame.sprite.Sprite):
         if self.rect.top < 0:
             self.rect.top = 0
 
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        pass
+
 # Define Wall class
 class Wall(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h, existing_walls=None):
@@ -85,6 +99,20 @@ class Wall(pygame.sprite.Sprite):
         if self.rect.top > HEIGHT:
             self.kill()
 
+def responwn_and_collect_coin(all_sprites, coins, player, chance_per_frame=0.005):
+    if random.random() < chance_per_frame:  # 0.2% chance every frame to spawn a coin
+        coin_x = random.randint(0, WIDTH - 20)
+        coin_y = random.randint(HEIGHT // 2, HEIGHT - 20)  # only on the bottom half
+        coin = Coin(coin_x, coin_y)
+        all_sprites.add(coin)
+        coins.add(coin)
+        
+    coin_hits = pygame.sprite.spritecollide(player, coins, True)  # The True means the coin will be deleted once collected
+    for coin in coin_hits:
+        global COIN_COUNTER
+        COIN_COUNTER += 1
+
+
 # Main game loop
 def game_loop():
     # Setup display and game title
@@ -101,6 +129,9 @@ def game_loop():
     running = True
     last_wall_y = 0                         # Track y-position of last generated wall
     start_time = datetime.now()
+    
+    coins = pygame.sprite.Group()
+    
     while running:
         # Calculate elapsed time to adjust wall speed dynamically
         current_time = datetime.now()
@@ -109,6 +140,8 @@ def game_loop():
         WALL_SPEED = 5 + elapsed_time/5
 
         clock.tick(FPS)  # Cap the game loop to the defined FPS
+
+        responwn_and_collect_coin(all_sprites, coins, player)
 
         # Generate new walls if there are fewer than 5 on screen
         if len(walls) < 5:
@@ -142,6 +175,9 @@ def game_loop():
         # Draw all sprites on screen and update the display
         screen.fill(BACKGROUND_COLOR)
         all_sprites.draw(screen)
+        font = pygame.font.SysFont(None, 36)  # Default font, size 36
+        text_surface = font.render(f"Coins: {COIN_COUNTER}", True, WHITE)
+        screen.blit(text_surface, (10, 10))  # Draw at top-left
         pygame.display.flip()
 
     pygame.quit()  # End the game
